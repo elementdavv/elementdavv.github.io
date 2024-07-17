d10aefc018305ccc563f19d1692305e96164c1f7
elementdavv <vinctai@gmail.com>
2024-07-17 11:02:51 +0800

    add hathitrust

---
+++ hathitrust/update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export CLASSPATH=bcprov-jdk18on-1.78.1.jar
+
+SERVER=hathitrust.org:443
+CERT=cert.pem
+STORE=store.bks
+
+if [ -a $CERT ]; then
+    rm $CERT || exit 1
+fi
+
+echo \
+    | openssl s_client -connect $SERVER 2>&1 \
+    | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' \
+    > $CERT
+
+if [ -a $STORE ]; then
+    rm $STORE || exit 1
+fi
+yes | keytool \
+      -import \
+      -v \
+      -trustcacerts \
+      -alias 0 \
+      -file $CERT \
+      -keystore $STORE \
+      -storetype BKS \
+      -provider org.bouncycastle.jce.provider.BouncyCastleProvider \
+      -providerpath bcprov-jdk18on-1.78.1.jar \
+      -storepass hathitrust.org
